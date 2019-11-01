600a02d30832c4eff72637c7075b79472a01a388
Will Wang <will.wang@mindbridge.ai>
2019-11-01 16:31:10 -0400

    init

---
+++ src/main/java/com/example/javann/JavaNnApplication.java
@@ -0,0 +1,13 @@
+package com.example.javann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javann/JavaNnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
